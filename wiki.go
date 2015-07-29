eb04cb6988eb9043b45fe0e4efd978c6125b4be0
leroi <leroidejesa@gmail.com>
2015-07-29 15:25:12 -0700

    add net/http pkg to serve test page

--- wiki.go
+++ wiki.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "io/ioutil"
+  "net/http"
 )
 
 // body element is []byte rather than string because that is the type expected by io libraries
@@ -25,9 +26,13 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/view/"):]
+  p, _ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-  p1.save()
-  p2, _ := loadPage("TestPage")
-  fmt.Println(string(p2.Body))
+  http.HandleFunc("/view/", viewHandler)
+  http.ListenAndServe(":8080", nil)
 }
