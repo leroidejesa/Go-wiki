389a85a5bd71180499e91498b42a5c630e265ab5
leroi <leroidejesa@gmail.com>
2015-07-30 11:25:20 -0700

    implement validation, refactor with func literals and closures

--- wiki.go
+++ wiki.go
@@ -4,10 +4,22 @@ import (
   "io/ioutil"
   "net/http"
   "html/template"
+  "regexp"
+  "errors"
+  "flag"
+  "net"
+  "log"
 )
+
+//validation
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 //template caching
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+var (
+  addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
+)
 
 // body element is []byte rather than string because that is the type expected by io libraries
 type Page struct {
@@ -36,8 +48,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
     }
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -46,8 +57,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     p = &Page{Title: title}
@@ -55,21 +65,58 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  err := p.save()
+  if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+//takes a function of viewHandler/editHandler/saveHandler type and returns a closure func (of type http.HandlerFunc)
+//(a closure encloses values defined outside of it)
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
     }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+    fn(w, r, m[2])
+  }
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil
 }
 
 func main() {
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  http.HandleFunc("/save/", saveHandler)
+  flag.Parse()
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
+
+  if *addr {
+    l, err := net.Listen("tcp", "127.0.0.1:0")
+    if err != nil {
+      log.Fatal(err)
+    }
+    err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644)
+    if err != nil {
+      log.Fatal(err)
+    }
+    s := &http.Server{}
+    s.Serve(l)
+    return
+  }
+
   http.ListenAndServe(":8080", nil)
 }
